08de8b898357866c3c49c33336360183ca492645
Maarten Brouwers <github.com@murb.nl>
2011-07-10 00:12:40 +0200

    Also non timezones were often matched

--- lib/chronic/time_zone.rb
+++ lib/chronic/time_zone.rb
@@ -8,7 +8,7 @@ module Chronic
     end
 
     def self.scan_for_all(token)
-      scanner = {/[PMCE][DS]T/i => :tz,
+      scanner = {/\b[PMCE][DS]T\b/i => :tz,
                  /(tzminus)?\d{4}/ => :tz}
       scanner.keys.each do |scanner_item|
         return self.new(scanner[scanner_item]) if scanner_item =~ token.word
